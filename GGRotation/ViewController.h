889de3eb6416bb8921cf867426fe58c4284159aa
otherplayer <osmk@qq.com>
2015-11-11 13:20:48 +0800

    Initial Commit

---
+++ GGRotation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GGRotation
+//
+//  Created by __无邪_ on 15/11/11.
+//  Copyright © 2015年 __无邪_. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
